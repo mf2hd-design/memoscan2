a87441dc55acd94f9d76e54d18ab3344abbc0999
mf2hd-design <weelilben@gmail.com>
2025-07-23 11:14:14 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,23 @@
-from quart import Quart, render_template, request, Response
+from flask import Flask, render_template, request, Response
 from scanner import run_full_scan_stream
 
-app = Quart(__name__)
+app = Flask(__name__)
 
 @app.route("/")
-async def index():
-    return await render_template("index.html")
+def index():
+    return render_template("index.html")
 
 @app.route("/scan")
-async def scan():
+def scan():
     url = request.args.get("url", "").strip()
 
-    async def _generate():
+    def _generate():
         if not url:
             yield "data: [ERROR] URL parameter is missing.\\n\\n"
             return
 
         print(f"[WebApp] Received scan request for: {url}")
-        async for data in run_full_scan_stream(url):
+        for data in run_full_scan_stream(url):
             yield data
 
     headers = {
